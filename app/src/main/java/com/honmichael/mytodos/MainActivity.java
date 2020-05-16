c3692640ebd3ac8094d66dbd90f01227734c3509
flawlessmiken <honmichael.5@gmail.com>
2020-05-16 02:16:06 +0100

    Personal Lesson on Android Architecural Component using Room ViewModel and Live Dara

---
+++ app/src/main/java/com/honmichael/mytodos/MainActivity.java
@@ -0,0 +1,21 @@
+package com.honmichael.mytodos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final WordListAdapter adapter = new WordListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
